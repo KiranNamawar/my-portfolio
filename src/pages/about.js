92c71de07a1202e4ac76cad67eec5202daf2363e
Kiran Namawar <kirannamawar@gmail.com>
2024-02-12 01:31:51 +0530

    Refactor index page component

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function AboutPage() {
+    return (
+        <main>
+            <h1>About Me</h1>
+        </main>
+    )
+}
+
+export const Head = () => <title>About | Kiran Namawar</title>
+
+export default AboutPage;
