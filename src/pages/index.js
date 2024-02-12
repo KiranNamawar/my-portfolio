e2a252124bfae5525398a59c550b67048d27563b
Kiran Namawar <kirannamawar@gmail.com>
2024-02-12 17:30:19 +0530

    Update image and add photo credit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,17 @@ function IndexPage() {
   return (
     <Layout pageTitle="Home Page">
       <p>Web Developer</p>
-      <StaticImage alt="skyline" src="" />
+      <StaticImage alt="" src="../images/background.jpg" />
+      <p>
+        Photo by
+        <a href="https://unsplash.com/@lulu09?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          拴 张
+        </a>
+        on
+        <a href="https://unsplash.com/photos/a-group-of-trees-covered-in-snow-next-to-a-forest-cUr8oSVN3NE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Unsplash
+        </a>
+      </p>
     </Layout>
   );
 }
