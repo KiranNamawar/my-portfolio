49488fbeaaaaf9b8830b94b4824925eb113ba293
Kiran Namawar <kirannamawar@gmail.com>
2024-02-12 17:22:38 +0530

    Refactor layout component and remove unused images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
-import * as React from 'react';
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image';
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 function IndexPage() {
   return (
     <Layout pageTitle="Home Page">
       <p>Web Developer</p>
-      <StaticImage
-        alt="skyline"
-        src="../images/skyline.jpg"
-      />
+      <StaticImage alt="skyline" src="" />
     </Layout>
-  )
+  );
 }
 
-export const Head = () => <title>Portfolio | Kiran Namawar</title>
+export const Head = () => <title>Portfolio | Kiran Namawar</title>;
 
 export default IndexPage;
